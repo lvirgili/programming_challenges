a66293d459647828a63c59e2d7edf2242c636524
Lucas Virgili <luvirgili@gmail.com>
2012-06-22 15:02:38 -0300

    spoj: TEST

---
+++ spoj/TEST.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int x;
+     while (true) {
+          cin >> x;
+          if (x == 42) break;
+          cout << x << endl;
+     }
+     return 0;
+}
