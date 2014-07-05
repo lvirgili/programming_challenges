1f059235079ca0cca400b29c161483b113fdf1ef
Lucas Virgili <luvirgili@gmail.com>
2014-07-05 00:39:37 -0300

    05/07/14

---
+++ a2/omar.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int x, y; cin >> x >> y;
+        cout << x+y << endl;
+    }
+    return 0;
+}
